f2cd0a6ad03f749b78a85d1095c978ab13e8715d
Tommy Hessel <thomas.hessel@duke.edu>
2019-06-20 12:07:46 +0100

    fuk me

--- src/rentals.js
+++ src/rentals.js
@@ -1,16 +1,35 @@
 import React from 'react'
+import axios from 'axios'
 
 class rentals extends React.Component {
     constructor(props){
       super(props)
       this.state = {
+          names: [],
+          stores: []
       }
     }
+
+    // componentDidMount(){
+    //     axios.get('https://randomuser.me/api/?results=10&inc=name,registered&nat=fr')
+    //     .then(json => console.log(json.data.results))
+    // }
+
+    componentDidMount(){
+        axios.get('https://randomuser.me/api/?results=10&inc=name,registered&nat=fr')
+        .then(json => json.data.results.map(result => (
+          {
+            names: `${result.name.first} ${result.name.last}`,
+            id: result.registered
+          })))
+        .then(newData => this.setState({names: newData, stores: newData}))
+        .catch(error => alert(error))
+      }
 
     render() {
         return (
           <div>
-        <h1>Rentals</h1>
+        <h1>Rentals {this.props.names}</h1>
         </div>
         );
       }
